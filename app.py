5e141f00e702e2debf10a965808abc83aa94d97c
DerikBrandon <161172852+DerikBrandon@users.noreply.github.com>
2024-09-04 20:56:53 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/send_message', methods=['POST'])
+def send_message():
+    name = request.form['name']
+    email = request.form['email']
+    message = request.form['message']
+    return f"Obrigado pela mensagem, {name}!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
